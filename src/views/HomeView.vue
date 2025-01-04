bad0fc19c49a35eb47f56509adce8cf77a793615
mdashraful <mdashrafulislam369@gmail.com>
2025-01-04 23:21:23 +0600

    home component setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HeroSection from '@/components/HeroSection.vue';
+import HomeCards from '@/components/HomeCards.vue';
+import Navbar from '@/components/Navbar.vue';
+import BlogListings from '@/components/BlogListings.vue';
+</script>
+
+<template>
+    <Navbar />
+    <HeroSection />
+    <HomeCards />    
+    <BlogListings />    
+</template>
